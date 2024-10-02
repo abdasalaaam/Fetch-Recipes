d940f61055985966d6f4b02c51060cdeb484da55
Abdasalaam Salem <abudsalem@gmail.com>
2024-10-01 23:46:31 -0500

    Initial Commit

---
+++ Fetch-RecipeApp/Fetch_RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_RecipeAppApp.swift
+//  Fetch-RecipeApp
+//
+//  Created by Abud Salem on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
